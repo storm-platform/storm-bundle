4f75a911450fe8b3c67434dcd1491935a7b8bbfd
Felipe Carlos <efelipecarlos@gmail.com>
2022-08-05 13:43:10 -0300

    environment: updating bootstrap scripts license

--- bootstrap/bootstrap-k8s.sh
+++ bootstrap/bootstrap-k8s.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (C) 2021 Storm Project
+#
+# storm-bundle is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
 
 #
 # Kind
